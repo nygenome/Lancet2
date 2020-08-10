e54f10379b7cdc4002da44e0741fa427dedc2c50
Rajeeva Musunuri <rmusunuri@nygenome.org>
2020-08-10 14:54:14 -0700

    feat: add option to use overlapping reads to build graph
    Former-commit-id: 5b1726ec748065ffeed10b322f38f29f2a526770
    Former-commit-id: b1e91d840d3ba375c2ef63dba1e89c89fc302877
    Former-commit-id: 71f25e18283c751a5f8796e6627e5c2fe09a308f

--- include/lancet/cli_params.h
+++ include/lancet/cli_params.h
@@ -97,5 +97,6 @@ class CliParams {
   bool tenxMode = false;          // NOLINT
   bool extractReadPairs = false;  // NOLINT
   bool noCtgCheck = false;        // NOLINT
+  bool useOverlapReads = false;   // NOLINT
 };
 }  // namespace lancet
