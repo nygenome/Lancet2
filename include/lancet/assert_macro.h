7e22aebe6b76588d7281a5cefc70360ae38d324d
Rajeeva Musunuri <rmusunuri@nygenome.org>
2020-08-12 18:36:17 -0700

    chore: add some context for failed assertion
    Former-commit-id: fa0efff8e46f5bd3d01b5a1b92edba556d57321a
    Former-commit-id: 90a7b5cb776d244def2d67f85646d029cd825e76
    Former-commit-id: 6cba8edab05ccfa87d3d35794f076f8e88f252f8

--- include/lancet/assert_macro.h
+++ include/lancet/assert_macro.h
@@ -2,9 +2,11 @@
 
 #ifndef NDEBUG
 #include <stdexcept>
+
+#include "absl/strings/str_format.h"
 // NOLINTNEXTLINE
 #define LANCET_ASSERT(condition) \
-  if (!(condition)) throw std::runtime_error("assertion failed!");
+  if (!(condition)) throw std::runtime_error(absl::StrFormat("assertion failed: %s:%d", __FILE__, __LINE__));
 #else
 
 #define LANCET_ASSERT(condition) (void)0  // NOLINT
