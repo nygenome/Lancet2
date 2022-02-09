68eaa03f9aa71519c790458a4a54cda9f8028db0
Rajeeva Musunuri <rmusunuri@nygenome.org>
2022-02-09 17:23:06 +0530

    chore: add local editor files

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -8,15 +8,19 @@ function ensure_deps() {
 }
 
 function main() {
+  # shellcheck disable=SC2155
   local readonly PROJECT_DIR=$(git rev-parse --show-toplevel)
   local readonly BUMP_KIND="${1:-patch}"
 
+  # shellcheck disable=SC2155
   local MOST_RECENT_TAG=$(git describe --abbrev=0 --tags)
   MOST_RECENT_TAG="${MOST_RECENT_TAG:-0.0.0}"
   echo "MOST_RECENT_TAG: ${MOST_RECENT_TAG}"
 
   echo "BUMP_KIND: ${BUMP_KIND}"
   echo "sembump --kind ${BUMP_KIND} ${MOST_RECENT_TAG}"
+  # shellcheck disable=SC2155
+  # shellcheck disable=SC2034
   local readonly NEW_VERSION=$(sembump --kind "${BUMP_KIND}" "${MOST_RECENT_TAG}")
 
   if [[ -z ${NEW_VERSION} ]]; then
@@ -24,10 +28,10 @@ function main() {
   fi
 
   echo "Bumping version from ${MOST_RECENT_TAG} to ${NEW_VERSION}"
-  git-chglog --next-tag ${NEW_VERSION} -o ${PROJECT_DIR}/CHANGELOG.md
-  git add ${PROJECT_DIR}/CHANGELOG.md
+  git-chglog --next-tag "${NEW_VERSION}" -o "${PROJECT_DIR}"/CHANGELOG.md
+  git add "${PROJECT_DIR}"/CHANGELOG.md
   git commit -vsm "chore: Bump version to ${NEW_VERSION}"
   git tag -m "${NEW_VERSION}" -sa "${NEW_VERSION}"
 }
 
-ensure_deps && main $1
+ensure_deps && main "$1"
