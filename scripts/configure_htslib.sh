0f4450668806da8df16fd2ff99f59eda6ae52adf
Rajeeva Musunuri <rmusunuri@nygenome.org>
2022-03-23 11:40:56 -0400

    chore: use cflags env var directly
    Former-commit-id: ad1d1ade02f9adae0e5affc5b4cf606c33062681
    Former-commit-id: 978a41bca1891ddb6d222bfcb2a76518ba19fe02
    Former-commit-id: 61d7a4db8689255e505337402a6d8eac6e1b2c4e

--- scripts/configure_htslib.sh
+++ scripts/configure_htslib.sh
@@ -4,10 +4,9 @@ set -e
 
 readonly ROOT_DIR="${1}"
 readonly C_COMPILER="${2}"
-readonly CFG_CFLAGS="-O3 -DNDEBUG"
 
 echo "  HTSLIB CONFIGURE ROOT_DIR : ${ROOT_DIR}"
 echo "HTSLIB CONFIGURE C_COMPILER : ${C_COMPILER}"
 
-env CC="${C_COMPILER}" CFLAGS="${CFG_CFLAGS}" "${ROOT_DIR}"/configure \
+env CC="${C_COMPILER}" CFLAGS="-O3 -DNDEBUG" "${ROOT_DIR}"/configure \
     --prefix="${ROOT_DIR}" --enable-libcurl --enable-s3 --enable-gcs --disable-plugins
