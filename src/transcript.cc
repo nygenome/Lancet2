98d610eb9cf934227f0ef56e45f61211b5cbf80e
Rajeeva Musunuri <rmusunuri@nygenome.org>
2022-07-08 20:30:07 -0400

    fix: typo in rev alt cov

--- src/transcript.cc
+++ src/transcript.cc
@@ -45,8 +45,8 @@ auto Transcript::VariantCov(SampleLabel label) const -> VariantHpCov {
   if (label == SampleLabel::TUMOR) {
     const auto refFwd = isSomatic ? static_cast<u16>(std::round(sampleCovs[1].GetMean(Allele::REF, Strand::FWD, false)))
                                   : sampleCovs[1].GetMinimum(Allele::REF, Strand::FWD, false);
-    const auto refRev = isSomatic ? static_cast<u16>(std::round(sampleCovs[1].GetMean(Allele::REF, Strand::FWD, false)))
-                                  : sampleCovs[1].GetMinimum(Allele::REF, Strand::FWD, false);
+    const auto refRev = isSomatic ? static_cast<u16>(std::round(sampleCovs[1].GetMean(Allele::REF, Strand::REV, false)))
+                                  : sampleCovs[1].GetMinimum(Allele::REF, Strand::REV, false);
 
     const auto altFwd = sampleCovs[1].GetMinimum(Allele::ALT, Strand::FWD, isSNV);
     const auto altRev = sampleCovs[1].GetMinimum(Allele::ALT, Strand::REV, isSNV);
@@ -71,8 +71,8 @@ auto Transcript::VariantCov(SampleLabel label) const -> VariantHpCov {
 
   const auto refFwd = isSomatic ? static_cast<u16>(std::round(sampleCovs[0].GetMean(Allele::REF, Strand::FWD, false)))
                                 : sampleCovs[0].GetMinimum(Allele::REF, Strand::FWD, false);
-  const auto refRev = isSomatic ? static_cast<u16>(std::round(sampleCovs[0].GetMean(Allele::REF, Strand::FWD, false)))
-                                : sampleCovs[0].GetMinimum(Allele::REF, Strand::FWD, false);
+  const auto refRev = isSomatic ? static_cast<u16>(std::round(sampleCovs[0].GetMean(Allele::REF, Strand::REV, false)))
+                                : sampleCovs[0].GetMinimum(Allele::REF, Strand::REV, false);
 
   const auto altFwd = isSNV ? sampleCovs[0].GetMinimum(Allele::ALT, Strand::FWD, true)
                             : sampleCovs[0].GetNonZeroMinimum(Allele::ALT, Strand::FWD, false);
