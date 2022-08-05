c5b33a2b7268927ab7e04751dbfee0aa34237764
ZhuBry <103147512+ZhuBry@users.noreply.github.com>
2022-08-05 10:22:10 -0400

    Update docusaurus.config.js
    Former-commit-id: 750da127c2649f28b00c64c60ae895d95d9b32c4
    Former-commit-id: 6618476aa46fb3ef5360448ed377121c81637e05
    Former-commit-id: d34bd9c983ffc615697d1e0eeb9aaeeae8bbdb38

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/nygenome/Lancet2/tree/main/',
+            'https://github.com/nygenome/Lancet2/tree/main/website',
         },
         blog: {
           showReadingTime: true,
