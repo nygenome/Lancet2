46cc7aff9130d3aa672f0acda4b47d8c7c92ebc7
ZhuBry <103147512+ZhuBry@users.noreply.github.com>
2022-08-10 10:00:10 -0400

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -132,7 +132,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Lancet. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
