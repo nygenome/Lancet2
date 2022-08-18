161bb9b8c7373b5df8ab5a71691461851e11ea9a
ZhuBry <103147512+ZhuBry@users.noreply.github.com>
2022-08-18 10:31:07 -0400

    Update docusaurus.config.js
    Former-commit-id: ef96a6d2671341352be35d99b0b2dfecdd2bab6d
    Former-commit-id: 42f97ba785cb71e577c49e10e3720214b94de4ef
    Former-commit-id: a8a40c9300eaca7c6de6213c5e2fa2d8fc10c594

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'Background',
             position: 'left',
             label: 'Tutorial',
           },
