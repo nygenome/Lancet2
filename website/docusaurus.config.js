81debea5cb71a57eaa0f3d5ab5f062993a1ffb24
Rajeeva Lochan Musunuri <2278936+omicsnut@users.noreply.github.com>
2022-09-06 10:23:51 -0400

    chore: Update docs Former-commit-id: 1c5ca07cc1317fba1cc7afef87f1c07df4489045 Former-commit-id: d47e8ce4a3cbf063556104d481c6438b5e529d29 Former-commit-id: ce29c3f75a97617c61c41edbd33902d1606b7d7c

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'getting_started',
+            docId: 'getting_started#installation',
             position: 'left',
             label: 'Docs',
           },
