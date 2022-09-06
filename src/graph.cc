173aed99fb86c859fc149b1e4c0ec6085ad57ca8
Rajeeva Lochan Musunuri <2278936+omicsnut@users.noreply.github.com>
2022-09-06 06:10:31 -0400

    fix: use public api to refer to transcipt member variable Former-commit-id: f8e1ebc84f3450960bbe7da1a1175204b3ca4dac Former-commit-id: d51fb61953e975c2ed71a5f9c3d031346e6bed41 Former-commit-id: 9cc4265b74796c1f7f3eae08e397890ed611b518

--- src/graph.cc
+++ src/graph.cc
@@ -534,7 +534,7 @@ void Graph::BuildVariants(absl::Span<const Transcript> transcripts, std::vector<
   absl::flat_hash_map<u64, AlleleSpan> alleleSpans;      // Maps canonical haplotype hash to allele span
 
   for (const Transcript& T : transcripts) {
-    if (T.refAllele == T.altAllele) continue;
+    if (T.RefSeq() == T.AltSeq()) continue;
 
     const auto isSNV = T.Code() == TranscriptCode::SNV;
     const auto alleles = T.GetAlleleHashes();
@@ -588,6 +588,8 @@ void Graph::BuildVariants(absl::Span<const Transcript> transcripts, std::vector<
 
   usize numVariants = 0;
   for (const Transcript& T : transcripts) {
+    if (T.RefSeq() == T.AltSeq()) continue;
+
     const auto isSNV = T.Code() == TranscriptCode::SNV;
     const auto alleles = T.GetAlleleHashes();
     const auto refCovs = sampleHapCovs.at(alleles.RefHash);
@@ -599,7 +601,6 @@ void Graph::BuildVariants(absl::Span<const Transcript> transcripts, std::vector<
     const auto nmlCov = VariantHpCov(refCovs.RawNmlCov, lowQualSNV ? altCovs.BQPassNmlCov : altCovs.RawNmlCov);
 
     if (Variant::ComputeState(tmrCov, nmlCov) == VariantState::NONE) continue;
-    if (T.refAllele == T.altAllele) continue;
 
     variants->emplace_back(Variant(T, kmerSize, tmrCov, nmlCov));
     numVariants++;
