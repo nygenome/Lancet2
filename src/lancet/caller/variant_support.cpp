6eea1da389db2bcfc09f0efe09bd9138df13a774
Rajeeva Lochan Musunuri <rmusunuri@nygenome.org>
2023-06-30 14:37:07 -0400

    fix hom ref and alt scores

--- src/lancet/caller/variant_support.cpp
+++ src/lancet/caller/variant_support.cpp
@@ -36,20 +36,37 @@ auto VariantSupport::ComputePLs() const -> std::array<int, 3> {
   const auto nref = static_cast<f64>(TotalRefCov());
   const auto nalt = static_cast<f64>(TotalAltCov());
   const auto total = static_cast<f64>(TotalSampleCov());
-  // NOLINTBEGIN(readability-braces-around-statements)
+
+  // NOLINTNEXTLINE(readability-braces-around-statements)
   if (nref == 0.0 && nalt == 0.0 && total == 0.0) return {0, 0, 0};
-  if (nref == 0.0) return {hts::MAX_PHRED_SCORE, hts::MAX_PHRED_SCORE, 0};
-  if (nalt == 0.0) return {0, hts::MAX_PHRED_SCORE, hts::MAX_PHRED_SCORE};
-  // NOLINTEND(readability-braces-around-statements)
 
-  const auto ref_fraction = static_cast<f64>(nref) / static_cast<f64>(total);
-  const auto alt_fraction = static_cast<f64>(nalt) / static_cast<f64>(total);
-  const boost::math::binomial_distribution<f64> ref_dist(total, ref_fraction);
-  const boost::math::binomial_distribution<f64> alt_dist(total, alt_fraction);
+  auto nhet_ref = nref;
+  auto nhet_alt = nalt;
+  auto ref_allele_prob = static_cast<f64>(nref) / static_cast<f64>(total);
+  auto alt_allele_prob = static_cast<f64>(nalt) / static_cast<f64>(total);
 
-  const auto prob_hom_ref = std::max(boost::math::pdf(ref_dist, total), boost::math::pdf(alt_dist, 0));
-  const auto prob_het_alt = std::max(boost::math::pdf(ref_dist, nref), boost::math::pdf(alt_dist, nalt));
-  const auto prob_hom_alt = std::max(boost::math::pdf(ref_dist, 0), boost::math::pdf(alt_dist, total));
+  static constexpr f64 HALF = 0.5;
+  const auto site_error_prob = ExpectedErrorProbabilityAtSite();
+
+  if (nref == 0.0) {
+    nhet_ref = total * HALF;
+    nhet_alt = total * HALF;
+    ref_allele_prob = site_error_prob;
+    alt_allele_prob = 1.0 - ref_allele_prob;
+  }
+
+  if (nalt == 0.0) {
+    nhet_alt = total * HALF;
+    nhet_ref = total * HALF;
+    alt_allele_prob = site_error_prob;
+    ref_allele_prob = 1.0 - alt_allele_prob;
+  }
+
+  const boost::math::binomial_distribution<f64> ref_dist(total, ref_allele_prob);
+  const boost::math::binomial_distribution<f64> alt_dist(total, alt_allele_prob);
+  const auto prob_hom_ref = boost::math::pdf(ref_dist, total) * boost::math::pdf(alt_dist, 0);
+  const auto prob_het_alt = boost::math::pdf(ref_dist, nhet_ref) * boost::math::pdf(alt_dist, nhet_alt);
+  const auto prob_hom_alt = boost::math::pdf(ref_dist, 0) * boost::math::pdf(alt_dist, total);
 
   return ConvertGtProbsToPls({prob_hom_ref, prob_het_alt, prob_hom_alt});
 }
@@ -68,7 +85,7 @@ auto VariantSupport::MeanHaplotypeQualities() const -> std::array<int, 2> {
   const auto ref_range = std::ranges::join_view(ref_quals);
   const auto alt_range = std::ranges::join_view(alt_quals);
 
-  static const auto summer = [](const f64 sum, const u8 bqual) -> f64 { return sum + hts::PhredToErrorProb(bqual); };
+  static const auto summer = [](const f64 sum, const u8 bql) { return sum + hts::PhredToErrorProb(bql); };
   const auto sum_ref_eprob = std::accumulate(ref_range.begin(), ref_range.end(), 0.0, summer);
   const auto sum_alt_eprob = std::accumulate(alt_range.begin(), alt_range.end(), 0.0, summer);
   const auto mean_ref_eprob = sum_ref_eprob == 0.0 ? 0.0 : sum_ref_eprob / static_cast<f64>(TotalRefCov());
@@ -77,12 +94,25 @@ auto VariantSupport::MeanHaplotypeQualities() const -> std::array<int, 2> {
   return {hts::ErrorProbToPhred(mean_ref_eprob), hts::ErrorProbToPhred(mean_alt_eprob)};
 }
 
+auto VariantSupport::ExpectedErrorProbabilityAtSite() const -> f64 {
+  // NOLINTNEXTLINE(readability-braces-around-statements)
+  if (TotalSampleCov() == 0) return 0.0;
+
+  const std::array<absl::Span<const u8>, 4> data{mRefFwdQuals, mRefRevQuals, mAltFwdQuals, mAltRevQuals};
+  const auto quals = std::ranges::join_view(data);
+
+  static const auto summer = [](const f64 sum, const u8 bql) { return sum + hts::PhredToErrorProb(bql); };
+  const auto err_prob_sum = std::accumulate(quals.begin(), quals.end(), 0.0, summer);
+  return err_prob_sum / static_cast<f64>(TotalSampleCov());
+}
+
 auto VariantSupport::ConvertGtProbsToPls(const std::array<f64, 3>& gt_probs) -> std::array<int, 3> {
   const auto [prob_hom_ref, prob_het_alt, prob_hom_alt] = gt_probs;
 
-  const f64 hom_ref_ll = prob_hom_ref == 0.0 ? 0.0 : std::log10(prob_hom_ref);
-  const f64 het_alt_ll = prob_het_alt == 0.0 ? 0.0 : std::log10(prob_het_alt);
-  const f64 hom_alt_ll = prob_hom_alt == 0.0 ? 0.0 : std::log10(prob_hom_alt);
+  static constexpr f64 min_log_prob = -308.0;
+  const f64 hom_ref_ll = prob_hom_ref == 0.0 ? min_log_prob : std::log10(prob_hom_ref);
+  const f64 het_alt_ll = prob_het_alt == 0.0 ? min_log_prob : std::log10(prob_het_alt);
+  const f64 hom_alt_ll = prob_hom_alt == 0.0 ? min_log_prob : std::log10(prob_hom_alt);
 
   static constexpr f64 LL_TO_PHRED_MULTIPLIER = -10.0;
   auto hom_ref_phred = LL_TO_PHRED_MULTIPLIER * hom_ref_ll;
@@ -91,11 +121,11 @@ auto VariantSupport::ConvertGtProbsToPls(const std::array<f64, 3>& gt_probs) -> 
 
   // Subtract minimum phred value to get normalized PL values
   const f64 min_phred = std::min({hom_ref_phred, het_alt_phred, hom_alt_phred});
-  const auto pl_hom_ref = hom_ref_phred - min_phred;
-  const auto pl_het_alt = het_alt_phred - min_phred;
-  const auto pl_hom_alt = hom_alt_phred - min_phred;
+  const auto pl_hom_ref = std::ceil(hom_ref_phred - min_phred);
+  const auto pl_het_alt = std::ceil(het_alt_phred - min_phred);
+  const auto pl_hom_alt = std::ceil(hom_alt_phred - min_phred);
 
-  return {hts::ClampPhredScore(pl_hom_ref), hts::ClampPhredScore(pl_het_alt), hts::ClampPhredScore(pl_hom_alt)};
+  return {static_cast<int>(pl_hom_ref), static_cast<int>(pl_het_alt), static_cast<int>(pl_hom_alt)};
 }
 
 }  // namespace lancet::caller
