5dd94c9fcd5e95e8db0f2a73103999907e4a0fc5
Rajeeva Lochan Musunuri <rmusunuri@nygenome.org>
2023-07-06 06:04:25 -0400

    truncate to 100 ms and us

--- src/lancet/cli/pipeline_runner.cpp
+++ src/lancet/cli/pipeline_runner.cpp
@@ -220,9 +220,9 @@ void PipelineRunner::Run() {
     const core::WindowPtr &curr_win = windows[async_worker_result.mGenomeIdx];
     const auto win_name = curr_win->ToSamtoolsRegion();
     const auto win_status = core::ToString(async_worker_result.mStatus);
-    const auto elapsed_time = absl::FormatDuration(absl::Trunc(timer.Runtime(), absl::Milliseconds(1)));
-    const auto rem_runtime = absl::FormatDuration(absl::Trunc(remtimer.EstimateRemaining(), absl::Milliseconds(1)));
-    const auto win_runtime = absl::FormatDuration(absl::Trunc(async_worker_result.mRuntime, absl::Microseconds(1)));
+    const auto elapsed_time = absl::FormatDuration(absl::Trunc(timer.Runtime(), absl::Milliseconds(100)));
+    const auto rem_runtime = absl::FormatDuration(absl::Trunc(remtimer.EstimateRemaining(), absl::Milliseconds(100)));
+    const auto win_runtime = absl::FormatDuration(absl::Trunc(async_worker_result.mRuntime, absl::Microseconds(100)));
     LOG_INFO("Progress: {:>8.4f}% | Elapsed: {} | ETA: {} @ {:.2f} windows/sec | {} done with {} in {}",
              percent_windows_done(num_total_windows - done_windows_counter->load(std::memory_order_acquire)),
              elapsed_time, rem_runtime, remtimer.MeanRatePerSecond(), win_name, win_status, win_runtime)
