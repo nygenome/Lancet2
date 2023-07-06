1c522baf729a8a70e413251c813ced0491f30682
Rajeeva Lochan Musunuri <rmusunuri@nygenome.org>
2023-07-06 07:42:42 -0400

    rename file

--- src/lancet/cli/eta_timer.cpp
+++ src/lancet/cli/eta_timer.cpp
@@ -1,23 +1,23 @@
-#include "lancet/cli/remaining_timer.h"
+#include "lancet/cli/eta_timer.h"
 
 #include <cmath>
 
 namespace lancet::cli {
 
-RemainingTimer::RemainingTimer(const usize num_iterations) : mNumTotal(num_iterations) {}
+EtaTimer::EtaTimer(const usize num_iterations) : mNumTotal(num_iterations) {}
 
-void RemainingTimer::Increment() {
+void EtaTimer::Increment() {
   mNumDone++;
   mRunStats.Add(absl::ToInt64Nanoseconds(mProgressTimer.Runtime()));
   mProgressTimer.Reset();
 }
 
-auto RemainingTimer::EstimateRemaining() const -> absl::Duration {
+auto EtaTimer::EstimatedEta() const -> absl::Duration {
   const auto estimated_ns_remaining = static_cast<f64>(mNumTotal - mNumDone) * mRunStats.Mean();
   return absl::Nanoseconds(estimated_ns_remaining);
 }
 
-auto RemainingTimer::MeanRatePerSecond() const -> f64 {
+auto EtaTimer::MeanRatePerSecond() const -> f64 {
   static constexpr f64 NS_TO_SECS = 1e-9;
   static constexpr f64 WINDOWS_PER_SECOND_CONVERTER = -1.0;
   return std::pow(mRunStats.Mean() * NS_TO_SECS, WINDOWS_PER_SECOND_CONVERTER);
