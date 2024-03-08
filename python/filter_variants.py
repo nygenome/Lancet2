0cf5575ea5277c19cd2950ad761e9aae68b0e062
Rajeeva Lochan Musunuri <rmusunuri@nygenome.org>
2024-03-08 10:27:53 -0500

    feat: update python deps and filter_variants script to use latest model

--- python/filter_variants.py
+++ python/filter_variants.py
@@ -4,13 +4,12 @@ import argparse
 import logging
 import math
 import pickle
-import sys
 import urllib.request
 
 import pysam
 import tqdm
 
-SOMATIC_URL = "https://storage.googleapis.com/lancet-ml-models/somatic_ebm_clf.v1.pkl"
+SOMATIC_URL = "https://storage.cloud.google.com/lancet-ml-models/somatic_ebm.lancet_6ef7ba445a.v1.pkl"
 
 
 def download_and_load_model(link):
@@ -18,181 +17,73 @@ def download_and_load_model(link):
         return pickle.loads(response.read())
 
 
+def pct_strand_imbalance(combined_cnt, fwd_strand_cnt):
+    try:
+        deviation = 0.5 - (fwd_strand_cnt / combined_cnt)
+        return round(deviation * 100, 2)
+    except ZeroDivisionError:
+        return 0
+
+
+def parse_sample_fields(variant, sidx, out_key_prefix):
+    data = {k: variant.samples[sidx][k] for k in variant.format.keys()}
+    data = {k: round(v, 2) if isinstance(v, float) else v for k, v in data.items()}
+
+    result = {
+        "DEPTH": data["DP"],
+        "REF_COUNT": data["AD"][0],
+        "ALT_COUNT": data["AD"][1],
+        "REF_RATIO": 0 if data["DP"] == 0 else round(data["AD"][0] / data["DP"], 3),
+        "ALT_RATIO": 0 if data["DP"] == 0 else round(data["AD"][1] / data["DP"], 3),
+        "REF_PCT_STRAND_BIAS": pct_strand_imbalance(data["AD"][0], data["ADF"][0]),
+        "ALT_PCT_STRAND_BIAS": pct_strand_imbalance(data["AD"][1], data["ADF"][1]),
+        "PCT_HQ_READS_IN_WINDOW": 0 if math.isinf(data["PRF"]) else round(data["PRF"] * 100, 2),
+    }
+
+    # Round 0.0 to 0, since VAF is rounded to 3 decimal points
+    result["REF_RATIO"] = 0 if result["REF_RATIO"] == 0 else result["REF_RATIO"]
+    result["ALT_RATIO"] = 0 if result["ALT_RATIO"] == 0 else result["ALT_RATIO"]
+
+    for k in ("RAQS", "AAQS", "RMQS", "AMQS", "RAPDS", "AAPDS"):
+        minimum, median, maximum, absdev = data[k]
+        out_prefix = k[:-1]
+        result.update(
+            {
+                f"{out_prefix}_MINIMUM": minimum,
+                f"{out_prefix}_MEDIAN": median,
+                f"{out_prefix}_MAXIMUM": maximum,
+                f"{out_prefix}_ABSDEV": absdev,
+            }
+        )
+
+    return {out_key_prefix + k: v for k, v in result.items()}
+
+
+def get_variant_state(v):
+    return "SHARED" if v.info.get("SHARED") else "NORMAL" if v.info.get("NORMAL") else "TUMOR"
+
+
+def build_variant_info(variant):
+    result = {
+        "VARIANT_STATE": get_variant_state(variant),
+        "VARIANT_TYPE": variant.info.get("TYPE"),
+        "KMER_LENGTH": variant.info.get("KMERLEN"),
+        "VARIANT_LENGTH": variant.info.get("LENGTH"),
+        "SOMATIC_FET_SCORE": round(variant.qual, 2),
+        "VARIANT_NEAR_STR": variant.info.get("STR"),
+    }
+
+    result.update(parse_sample_fields(variant, 0, "NML_"))
+    result.update(parse_sample_fields(variant, 1, "TMR_"))
+
+    vaf_diff = round(result["TMR_ALT_RATIO"] - result["NML_ALT_RATIO"], 2)
+    result.update(ABS_TMR_NML_VAF_DIFF=abs(vaf_diff))
+
+    return result
+
+
 def phred_score(error_probability):
     return round(-10 * math.log10(error_probability), 2)
-
-
-def build_variant_data(variant, normal_sample_idx=0, tumor_sample_idx=1):
-    smpls = [i for i in variant.samples.itervalues()]
-
-    NML = normal_sample_idx
-    TMR = tumor_sample_idx
-    REF = 0
-    ALT = 1
-
-    nml_depth = smpls[NML]["DP"]
-    nml_ref_cnt = smpls[NML]["AD"][REF]
-    nml_fwd_ref_cnt = smpls[NML]["ADF"][REF]
-    nml_alt_cnt = smpls[NML]["AD"][ALT]
-    nml_fwd_alt_cnt = smpls[NML]["ADF"][ALT]
-
-    tmr_depth = smpls[TMR]["DP"]
-    tmr_ref_cnt = smpls[TMR]["AD"][REF]
-    tmr_fwd_ref_cnt = smpls[TMR]["ADF"][REF]
-    tmr_alt_cnt = smpls[TMR]["AD"][ALT]
-    tmr_fwd_alt_cnt = smpls[TMR]["ADF"][ALT]
-
-    data = [
-        # REF_ALLELE_LENGTH
-        len(variant.ref),
-        # ALT_ALLELE_LENGTH
-        len(variant.alts[0]),
-        # IS_SHARED_VARIANT
-        variant.info.get("SHARED", False),
-        # IS_TUMOR_ONLY_VARIANT
-        variant.info.get("TUMOR", False),
-        # VARIANT_LENGTH
-        variant.info.get("LENGTH"),
-        # KMER_LENGTH
-        variant.info.get("KMERLEN"),
-        # IS_STR_VARIANT
-        variant.info.get("STR", False),
-        # STR_REGION_LENGTH
-        variant.info.get("STR_LEN", 0),
-        # STR_MOTIF_LENGTH
-        len(variant.info.get("STR_MOTIF", "")),
-        # NORMAL_DEPTH
-        nml_depth,
-        # NORMAL_ALT_COUNT_BY_DEPTH
-        0 if nml_depth == 0 else nml_alt_cnt / nml_depth,
-        # NORMAL_REF_PCT_STRAND_IMBALANCE
-        round(abs((0.5 - (0.5 if nml_ref_cnt == 0 else nml_fwd_ref_cnt / nml_ref_cnt)) * 100), 2),
-        # NORMAL_ALT_PCT_STRAND_IMBALANCE
-        round(abs((0.5 - (0.5 if nml_alt_cnt == 0 else nml_fwd_alt_cnt / nml_alt_cnt)) * 100), 2),
-        # NORMAL_PCT_FAIL_READS_IN_WINDOW
-        round(100 - (100 * smpls[NML]["PRF"]), 2),
-        # NORMAL_REF_ALLELE_QUALITY_MINIMUM
-        smpls[NML]["RAQS"][0],
-        # NORMAL_ALT_ALLELE_QUALITY_MINIMUM
-        smpls[NML]["AAQS"][0],
-        # NORMAL_REF_ALLELE_QUALITY_MEDIAN
-        smpls[NML]["RAQS"][1],
-        # NORMAL_ALT_ALLELE_QUALITY_MEDIAN
-        smpls[NML]["AAQS"][1],
-        # NORMAL_REF_ALLELE_QUALITY_MAXIMUM
-        smpls[NML]["RAQS"][2],
-        # NORMAL_ALT_ALLELE_QUALITY_MAXIMUM
-        smpls[NML]["AAQS"][2],
-        # NORMAL_REF_ALLELE_QUALITY_ABSDEV
-        smpls[NML]["RAQS"][3],
-        # NORMAL_ALT_ALLELE_QUALITY_ABSDEV
-        smpls[NML]["AAQS"][3],
-        # NORMAL_REF_MAPPING_QUALITY_MINIMUM
-        smpls[NML]["RMQS"][0],
-        # NORMAL_ALT_MAPPING_QUALITY_MINIMUM
-        smpls[NML]["AMQS"][0],
-        # NORMAL_REF_MAPPING_QUALITY_MEDIAN
-        smpls[NML]["RMQS"][1],
-        # NORMAL_ALT_MAPPING_QUALITY_MEDIAN
-        smpls[NML]["AMQS"][1],
-        # NORMAL_REF_MAPPING_QUALITY_MAXIMUM
-        smpls[NML]["RMQS"][2],
-        # NORMAL_ALT_MAPPING_QUALITY_MAXIMUM
-        smpls[NML]["AMQS"][2],
-        # NORMAL_REF_MAPPING_QUALITY_ABSDEV
-        smpls[NML]["RMQS"][3],
-        # NORMAL_ALT_MAPPING_QUALITY_ABSDEV
-        smpls[NML]["AMQS"][3],
-        # NORMAL_REF_ALN_PCT_DIFF_MINIMUM
-        smpls[NML]["RAPDS"][0],
-        # NORMAL_ALT_ALN_PCT_DIFF_MINIMUM
-        smpls[NML]["AAPDS"][0],
-        # NORMAL_REF_ALN_PCT_DIFF_MEDIAN
-        smpls[NML]["RAPDS"][1],
-        # NORMAL_ALT_ALN_PCT_DIFF_MEDIAN
-        smpls[NML]["AAPDS"][1],
-        # NORMAL_REF_ALN_PCT_DIFF_MAXIMUM
-        smpls[NML]["RAPDS"][2],
-        # NORMAL_ALT_ALN_PCT_DIFF_MAXIMUM
-        smpls[NML]["AAPDS"][2],
-        # NORMAL_REF_ALN_PCT_DIFF_ABSDEV
-        smpls[NML]["RAPDS"][3],
-        # NORMAL_ALT_ALN_PCT_DIFF_ABSDEV
-        smpls[NML]["AAPDS"][3],
-        # TUMOR_DEPTH
-        tmr_depth,
-        # TUMOR_ALT_COUNT_BY_DEPTH
-        0 if tmr_depth == 0 else tmr_alt_cnt / tmr_depth,
-        # TUMOR_REF_PCT_STRAND_IMBALANCE
-        round(abs((0.5 - (0.5 if tmr_ref_cnt == 0 else tmr_fwd_ref_cnt / tmr_ref_cnt)) * 100), 2),
-        # TUMOR_ALT_PCT_STRAND_IMBALANCE
-        round(abs((0.5 - (0.5 if tmr_alt_cnt == 0 else tmr_fwd_alt_cnt / tmr_alt_cnt)) * 100), 2),
-        # TUMOR_PCT_FAIL_READS_IN_WINDOW
-        round(100 - (100 * smpls[TMR]["PRF"]), 2),
-        # TUMOR_REF_ALLELE_QUALITY_MINIMUM
-        smpls[TMR]["RAQS"][0],
-        # TUMOR_ALT_ALLELE_QUALITY_MINIMUM
-        smpls[TMR]["AAQS"][0],
-        # TUMOR_REF_ALLELE_QUALITY_MEDIAN
-        smpls[TMR]["RAQS"][1],
-        # TUMOR_ALT_ALLELE_QUALITY_MEDIAN
-        smpls[TMR]["AAQS"][1],
-        # TUMOR_REF_ALLELE_QUALITY_MAXIMUM
-        smpls[TMR]["RAQS"][2],
-        # TUMOR_ALT_ALLELE_QUALITY_MAXIMUM
-        smpls[TMR]["AAQS"][2],
-        # TUMOR_REF_ALLELE_QUALITY_ABSDEV
-        smpls[TMR]["RAQS"][3],
-        # TUMOR_ALT_ALLELE_QUALITY_ABSDEV
-        smpls[TMR]["AAQS"][3],
-        # TUMOR_REF_MAPPING_QUALITY_MINIMUM
-        smpls[TMR]["RMQS"][0],
-        # TUMOR_ALT_MAPPING_QUALITY_MINIMUM
-        smpls[TMR]["AMQS"][0],
-        # TUMOR_REF_MAPPING_QUALITY_MEDIAN
-        smpls[TMR]["RMQS"][1],
-        # TUMOR_ALT_MAPPING_QUALITY_MEDIAN
-        smpls[TMR]["AMQS"][1],
-        # TUMOR_REF_MAPPING_QUALITY_MAXIMUM
-        smpls[TMR]["RMQS"][2],
-        # TUMOR_ALT_MAPPING_QUALITY_MAXIMUM
-        smpls[TMR]["AMQS"][2],
-        # TUMOR_REF_MAPPING_QUALITY_ABSDEV
-        smpls[TMR]["RMQS"][3],
-        # TUMOR_ALT_MAPPING_QUALITY_ABSDEV
-        smpls[TMR]["AMQS"][3],
-        # TUMOR_REF_ALN_PCT_DIFF_MINIMUM
-        smpls[TMR]["RAPDS"][0],
-        # TUMOR_ALT_ALN_PCT_DIFF_MINIMUM
-        smpls[TMR]["AAPDS"][0],
-        # TUMOR_REF_ALN_PCT_DIFF_MEDIAN
-        smpls[TMR]["RAPDS"][1],
-        # TUMOR_ALT_ALN_PCT_DIFF_MEDIAN
-        smpls[TMR]["AAPDS"][1],
-        # TUMOR_REF_ALN_PCT_DIFF_MAXIMUM
-        smpls[TMR]["RAPDS"][2],
-        # TUMOR_ALT_ALN_PCT_DIFF_MAXIMUM
-        smpls[TMR]["AAPDS"][2],
-        # TUMOR_REF_ALN_PCT_DIFF_ABSDEV
-        smpls[TMR]["RAPDS"][3],
-        # TUMOR_ALT_ALN_PCT_DIFF_ABSDEV
-        smpls[TMR]["AAPDS"][3],
-        # "TUMOR_NORMAL_VAF_ABSDIFF"
-        abs(smpls[TMR]["VAF"] - smpls[NML]["VAF"]),
-    ]
-
-    return data
-
-
-def make_somatic_variant_list(raw_vcf_path):
-    data = list()
-    for v in tqdm.tqdm(pysam.VariantFile(raw_vcf_path)):
-        # Skip adding NORMAL only variants,
-        # since they don't need to be eval'ed
-        # by the somatic machine learning model
-        if not v.info.get("NORMAL", False):
-            data.append(build_variant_data(v))
-    return data
 
 
 def main(raw_vcf_path):
@@ -200,42 +91,38 @@ def main(raw_vcf_path):
     msg_fmt = "%(asctime)s | %(levelname)s | %(message)s"
     dt_fmt = "%Y-%m-%d %H:%M:%S"
     logging.basicConfig(format=msg_fmt, level=logging.INFO, datefmt=dt_fmt)
+    logging.info(f"Starting to filter and score input VCF {raw_vcf_path}")
 
-    logging.info("Building SHARED/TUMOR variants dataframe for further evaluation")
-    variants = make_somatic_variant_list(raw_vcf_path)
-    logging.info(f"Done building dataframe with {len(variants)} SHARED/TUMOR variants")
+    logging.info(f"Building variants dataframe for ML model evaluation")
+    variants_itr = tqdm.tqdm(pysam.VariantFile(raw_vcf_path, threads=2))
+    df = [tuple(build_variant_info(v).values()) for v in variants_itr]
+    logging.info(f"Done building dataframe with {len(df)} variants")
 
-    logging.info("Loading somatic machine learning model into memory")
+    logging.info("Loading somatic ML model into memory")
     ml_model = download_and_load_model(SOMATIC_URL)
+    logging.info("Done loading somatic ML model into memory")
 
-    logging.info("Applying machine learning model to the variants dataframe")
-    preds = ml_model.predict(variants)
-    probs = ml_model.predict_proba(variants)
-    logging.info("Done applying machine learning model to the variants dataframe")
+    logging.info("Applying somatic ML model to the variants dataframe")
+    predictions = ml_model.predict(df)
+    probabilities = ml_model.predict_proba(df)
+    logging.info("Done applying somatic ML model to the variants dataframe")
 
-    logging.info("Writing final filtered and scored output VCF")
-    invcf = pysam.VariantFile(raw_vcf_path)
+    logging.info("Writing filtered and scored variants in VCF format to stdout")
+    invcf = pysam.VariantFile(raw_vcf_path, threads=2)
     outvcf = pysam.VariantFile("-", "w", header=invcf.header)
-    max_qual = phred_score(sys.float_info.min)
 
-    idx_ml_model = -1
-    for v in tqdm.tqdm(invcf):
-        if v.info.get("NORMAL", False):
-            continue
-
-        idx_ml_model += 1
-        # Use lowest probablity class of machine learning binary
-        # classification as error probability for phred score
-        err_prob = sorted(probs[idx_ml_model])[0]
-        is_pass_variant = preds[idx_ml_model]
+    for idx, v in tqdm.tqdm(enumerate(invcf)):
+        is_pass_variant = predictions[idx]
         if is_pass_variant:
-            v.qual = max_qual if err_prob == 0 else phred_score(err_prob)
+            class_scores = probabilities[idx]
+            err_prob = sorted(class_scores)[0]
+            v.qual = phred_score(err_prob)
             v.filter.add("PASS")
             outvcf.write(v)
 
     invcf.close()
     outvcf.close()
-    logging.info("Done writing final filtered output VCF")
+    logging.info(f"Done writing filtered and scored variants to output VCF")
 
 
 if __name__ == "__main__":
