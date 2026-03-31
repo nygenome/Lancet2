f0c1ecf139ebf4fb3beee110dd5504a44bd55821
Rajeeva Musunuri <rmusunuri@nygenome.org>
2026-03-31 15:05:29 -0400

    chore: use different semver helper tool

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,7 +4,7 @@
 set -e
 
 function ensure_deps() {
-  go install github.com/threecommaio/sembump@latest
+  go install github.com/flaticols/bump@latest
   go install github.com/git-chglog/git-chglog/cmd/git-chglog@latest
 }
 
@@ -12,23 +12,24 @@ function main() {
   local PROJECT_DIR=$(git rev-parse --show-toplevel)
   local BUMP_KIND="${1:-patch}"
 
-  local MOST_RECENT_TAG=$(git describe --abbrev=0 --tags | cut -d'.' -f1-3)
-  MOST_RECENT_TAG="${MOST_RECENT_TAG:-0.0.0}"
-  echo "MOST_RECENT_TAG VERSION: ${MOST_RECENT_TAG}"
+  #local MOST_RECENT_TAG=$(git describe --abbrev=0 --tags | cut -d'.' -f1-3)
+  #MOST_RECENT_TAG="${MOST_RECENT_TAG:-0.0.0}"
+  #echo "MOST_RECENT_TAG VERSION: ${MOST_RECENT_TAG}"
 
   echo "BUMP_KIND: ${BUMP_KIND}"
-  echo "sembump ${MOST_RECENT_TAG} ${BUMP_KIND}"
-  local NEW_VERSION=$(sembump "${MOST_RECENT_TAG}"  "${BUMP_KIND}")
+  #echo "sembump ${MOST_RECENT_TAG} ${BUMP_KIND}"
+  #local NEW_VERSION=$(sembump "${MOST_RECENT_TAG}"  "${BUMP_KIND}")
 
-  if [[ -z ${NEW_VERSION} ]]; then
-      exit 1
-  fi
+  #if [[ -z ${NEW_VERSION} ]]; then
+  #    exit 1
+  #fi
 
-  echo "Bumping version from ${MOST_RECENT_TAG} to ${NEW_VERSION}"
-  git-chglog --next-tag "${NEW_VERSION}" -o "${PROJECT_DIR}"/CHANGELOG.md
-  git add "${PROJECT_DIR}"/CHANGELOG.md
-  git commit -vsm "chore: Bump version to ${NEW_VERSION}"
-  git tag -m "${NEW_VERSION}" -sa "${NEW_VERSION}"
+  #echo "Bumping version from ${MOST_RECENT_TAG} to ${NEW_VERSION}"
+  git push && bump "${BUMP_KIND}"
+  git-chglog -o "${PROJECT_DIR}"/CHANGELOG.md
+  #git add "${PROJECT_DIR}"/CHANGELOG.md
+  git commit -vsm "chore: Update release notes in changelog"
+  #git tag -m "${NEW_VERSION}" -sa "${NEW_VERSION}"
 }
 
 ensure_deps && main "$1"
