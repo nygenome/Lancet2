d828118b1dfea27ae571934c5ee81c412eccf3c2
Rajeeva Musunuri <rmusunuri@nygenome.org>
2026-03-31 15:14:07 -0400

    chore: minor fixes to bump helper script

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -26,7 +26,7 @@ function main() {
 
   #echo "Bumping version from ${MOST_RECENT_TAG} to ${NEW_VERSION}"
   git push && bump "${BUMP_KIND}" && git-chglog --config "${PROJECT_DIR}/.chglog/config.yml" -o "${PROJECT_DIR}"/CHANGELOG.md && \
-  git add "${PROJECT_DIR}"/CHANGELOG.md && git commit -vsm "chore: Update release notes in changelog"
+  git add "${PROJECT_DIR}"/CHANGELOG.md && git commit -vsm "chore: Update release notes in changelog" && git push --tags
   #git tag -m "${NEW_VERSION}" -sa "${NEW_VERSION}"
 }
 
